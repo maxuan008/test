e6faf1a0b9750c32d9c755f84e61f32b692c2c3d
mx <3132180492@qq.com>
2018-06-10 10:53:29 +0800

    promise

--- routes/test/testIndex.js
+++ routes/test/testIndex.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var config = require('../../config/config.json');
 var EventProxy =   require('eventproxy');
+//var Promise =  require('Promise');
 
 var env = global.ENV;
 console.log('test ENV:', env);
@@ -128,11 +129,83 @@ function epTest(req,res,next){
         
     /*2.分析ep的代码实现 */
  
-        ep.bind('e1',function(data1){
-            console.log('e1回掉了:', data1,ep);
-        });
+        // ep.bind('e1',function(data1){
+        //     console.log('e1回掉了:', data1,ep);
+        // });
+        // ep.emit('e1', '传递给e1的数据')
 
-        ep.emit('e1', '传递给e1的数据') 
+
+     /*3.语法分析 */
+        //一.包围函数
+            // (function(i,n){
+            //      console.log("包围函数:",i,n);
+            //      var t=new n; t.newStr('new Value;'); 
+            // })('HH', function(){
+            //      this.str='6'; 
+            //      this.newStr = function(newString){ this.str=newString; console.log("newString:",newString);  } 
+            //      return this;
+            // });
+
+        //二.process.nextTick 用法：
+        //Node.js是单线程的，除了系统IO之外，在它的事件轮询过程中，同一时间只会处理一个事件。把事件轮询想象成一个大的队列，在每个时间点上，系统只会处理一个事件。
+        //即使你的电脑有多个CPU核心，你也无法同时并行的处理多个事件。但也就是这种特性使得node.js适合处理I／O型的应用，不适合那种CPU运算型的应用。
+        //在每个I／O型的应用中，你只需要给每一个输入输出定义一个回调函数即可，他们会自动加入到事件轮询的处理队列里
+        //当I／O操作完成后，这个回调函数会被触发。然后系统会继续处理其他的请求。
+            // function foo()  {
+            //     console.error('foo：',arguments);
+            //     //return callback();
+            // }
+            // //foo('a','b');
+            // process.nextTick(()=>{  foo(1,2);  });
+            // //foo('a','b');
+            // var a=5;
+            // console.error('bar');
+                //输出结果： foo bar  
+
+
+
+        //三：promise用法: promise的then()方法是开始执行回调；完成后返回一个新promise对象。
+        //做饭
+        function cook(){
+            console.log('开始做饭。');
+            var p = new Promise(function(resolve, reject){        //做一些异步操作
+                setTimeout(function(){
+                    console.log('做饭完毕！');
+                    resolve('鸡蛋炒饭');
+                }, 1000);
+            });
+            return p;
+        }
+       
+        //吃饭
+        function eat(data){
+            console.log('开始吃饭：' + data);
+            var p = new Promise(function(resolve, reject){        //做一些异步操作
+                setTimeout(function(){
+                    console.log('吃饭完毕!');
+                    resolve('一块碗和一双筷子');
+                }, 2000);
+            });
+            return p;
+        }
+
+        function wash(data){
+            console.log('开始洗碗：' + data);
+            var p = new Promise(function(resolve, reject){        //做一些异步操作
+                setTimeout(function(){
+                    console.log('洗碗完毕!');
+                    resolve('干净的碗筷');
+                }, 2000);
+            });
+            return p;
+        }
+
+        cook()
+        .then(eat)
+        .then(wash)
+        .then(function(data){
+            console.log(data);
+        });
 
 
 
@@ -144,7 +217,14 @@ function epTest(req,res,next){
     "<br><br> 二：分析ep的代码实现 "+ 
     "<br>  1. 当有事件绑定时, ep会将函数以参数传入addListener方法,并将函数对象(callback)存储在 _callbacks （JSON）数据中 " + 
     "<br>  2. 随后ep对象执行emit函数（调用trigger），ep会从 _callbacks 中提取对应的事件函数， 并通过函数对象的 .apply方法对回调函数传参数并执行 " + 
-    "<br>  ";
+    "<br><br> 语法分析：<br>一：包围函数： (function(i,n){ xxx })(i,n); 第二个括号为立即执行并传递参数i,n ,作用是可以用它创建命名空间，只要把自己所有的代码都写在这个特殊的函数包装内，那么外部就不能访问，避免污染外部空间。  " + 
+    "<br>二：js的prototype，是实现面向对象的一个重要机制。每个函数也是一个对象，它们对应的类就是function，每个函数对象都具有一个子对象prototype prototype表示了一个类的属性的集合。当通过new来生成一个类的对象时，prototype对象的属性就会成为实例化对象的属性。" +
+    "<br>  ***   EventProxy.prototype.bindForAll = function (callback) {this.bind(ALL_EVENT, callback);}   ***  "+
+    "<br><br>三.arguments为函数的一个参数的JSON数据" +
+    "<br><br>四.Promise用法   " + 
+    "<br> 1.promise的then()方法是开始执行回调；完成后返回一个新promise对象； " +
+    "<br> 2.resolve(data)是返回成功后的状态并将data参数网下个函数传递  "
+
 
     return res.send( result);
 
